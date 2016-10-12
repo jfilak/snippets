ef38c71064e4aec854e04bb8b23ab78c471e4756
Jakub Filak <jfilak@redhat.com>
2016-10-12 15:42:31 +0200

    seek back in stream opened by open_memstream()
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

---
+++ memstream/main.c
@@ -0,0 +1,46 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
+
+enum {
+    TEST_NO_FLAGS = 0x0,
+    TEST_PUTS_EMPTY_STRING = 0x1,
+};
+
+void test(int flags)
+{
+    char *buffer = NULL;
+    size_t wrote = 0;
+
+    FILE *stream = open_memstream(&buffer, &wrote);
+    if (stream == NULL) {
+        err(EXIT_FAILURE, "open_memstream");
+    }
+
+    fprintf(stream, "%s", "Hello, world!");
+    fprintf(stream, "%s", "Dlanod");
+
+    if (fseek(stream, strlen("Hello, world!"), SEEK_SET) < 0) {
+        err(EXIT_FAILURE, "fseek");
+    }
+
+    if (flags & TEST_PUTS_EMPTY_STRING) {
+        fputs("", stream);
+    }
+
+    fclose(stream);
+
+    printf("\t'%s'\n", buffer);
+}
+
+int main(void)
+{
+    printf("Seek back:\n");
+    test(TEST_NO_FLAGS);
+
+    printf("Seek back and fputs() empty string:\n");
+    test(TEST_PUTS_EMPTY_STRING);
+
+    return EXIT_SUCCESS;
+}
