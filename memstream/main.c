6d37e6354dbe0b1ffce57fbbeb1ff83630d6c5e8
Jakub Filak <jfilak@redhat.com>
2016-10-13 11:03:32 +0200

    memstream: test flushing
    Signed-off-by: Jakub Filak <jfilak@redhat.com>

--- memstream/main.c
+++ memstream/main.c
@@ -6,6 +6,7 @@
 enum {
     TEST_NO_FLAGS = 0x0,
     TEST_PUTS_EMPTY_STRING = 0x1,
+    TEST_PUTS_FOO_STRING = 0x2,
 };
 
 void test(int flags)
@@ -29,9 +30,17 @@ void test(int flags)
         fputs("", stream);
     }
 
+    if (flags & TEST_PUTS_FOO_STRING) {
+        fputs("foo", stream);
+    }
+
+    fflush(stream);
+
+    printf("flushed - len = %2zd : '%s'\n", wrote, buffer);
+
     fclose(stream);
 
-    printf(" - len = %2zd : '%s'\n", wrote, buffer);
+    printf("closed  - len = %2zd : '%s'\n", wrote, buffer);
 
     free(buffer);
 }
@@ -43,6 +52,9 @@ int main(void)
 
     printf("Seek back and fputs() empty string:\n");
     test(TEST_PUTS_EMPTY_STRING);
+
+    printf("Seek back and fputs() foo string:\n");
+    test(TEST_PUTS_FOO_STRING);
 
     return EXIT_SUCCESS;
 }
